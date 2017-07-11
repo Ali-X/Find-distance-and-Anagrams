6b81d164b9ea0ccdfa8aa559f36c44aacab6cdbd
Alina Neshchebnaya <alina240895@yandex.ru>
2017-07-11 11:18:40 +0300

    Refactoring
    Lines (17-22)

--- HomeTask_1_2/src/Main.java
+++ HomeTask_1_2/src/Main.java
@@ -7,15 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         reverse(reader.readLine());
-
     }
 
-    private static void reverse(String word){
+    private static void reverse(String word) {
         int start = 0;
         int end = 0;
+        boolean isEnd = false;
         for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == ' ' || i == word.length()-1) {
-                end = (i == word.length()-1) ? i : i - 1;
+            if (i == word.length() - 1) isEnd = true;
+            if (word.charAt(i) == ' ' || isEnd) {
+                if (isEnd)
+                    end = i;
+                else
+                    end = i - 1;
                 while (end >= start) {
                     System.out.print(word.charAt(end));
                     --end;
@@ -23,6 +27,7 @@ public class Main {
                 start = i + 1;
                 System.out.print(" ");
             }
+
         }
     }
 }
